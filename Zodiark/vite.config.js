e56c0c2a620a8c9dfcaabcb81ddd68f1a6e185e7
Souma-Sumire <553469159@qq.com>
2023-07-06 19:47:37 +0800

    Update vite.config.js

--- Zodiark/vite.config.js
+++ Zodiark/vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Zodiark/",
+  base: "/EchoOverlay/Zodiark/",
   build: {
     outDir: "./dist",
     rollupOptions: {
